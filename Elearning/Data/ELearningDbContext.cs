7f4631e1c89557d5c16b58860c9c1080519360ce
magicodes1 <minhsangandonly1@gmail.com>
2022-06-02 11:46:25 +0700

    Add DbContext for those Entity Model Classes before.

---
+++ Elearning/Data/ELearningDbContext.cs
@@ -0,0 +1,24 @@
+using ElearningApplication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElearningApplication.Data;
+
+public class ELearningDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ELearningDbContext(DbContextOptions<ELearningDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Grade> Grades { get; set; } = null!;
+    public DbSet<Course> Courses { get; set; } = null!;
+    public DbSet<Term> Terms { get; set; } = null!;
+    public DbSet<Subject> Subjects { get; set; } = null!;
+    public DbSet<OriginClass> OriginClasses { get; set; } = null!;
+    public DbSet<OnlineClass> OnlineClasses { get; set; } = null!;
+    public DbSet<SubjectSchedule> SubjectSchedules { get; set; } = null!;
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
+
+    
+}
